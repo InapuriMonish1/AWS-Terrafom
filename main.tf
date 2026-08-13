05f62a2e35b9acc54ab1dab4b98ce7130e47de35
Monish Inapuri <monish.inapuri@valuemomentum.com>
2026-08-13 12:52:24 +0530

    Changed errors which caused terrafrom fail

--- main.tf
+++ main.tf
@@ -85,9 +85,9 @@ resource "aws_iam_role_policy" "lambda_dynamodb_policy" {
 data "archive_file" "lambda_zip" {
   type        = "zip"
 
-  source_file = "${path.module}/Lambda/claims-lambda.py"
+  source_file = "${path.module}/Lambda/lambda_function.py"
 
-  output_path = "${path.module}/claims-lambda.zip"
+  output_path = "${path.module}/lambda_function.zip"
 }
 
 
